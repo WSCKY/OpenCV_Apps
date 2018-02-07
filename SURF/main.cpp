f01d90ad04fadbe91f2b5ee1e2ff890d3ad7c39a
kyChu <hello.kychu@gmail.com>
2018-02-07 20:50:33 +0800

    create new test code

--- SURF/main.cpp
+++ SURF/main.cpp
@@ -1,7 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <iostream>
-#include "opencv2/highgui.hpp"
 #include "opencv2/videoio.hpp"
 
 #include "opencv2/xfeatures2d.hpp"
@@ -10,72 +9,67 @@ using namespace cv;
 using namespace std;
 using namespace cv::xfeatures2d;
 
-//主函数
-int main()
+int main(void)
 {
-    //载入源图像
-    Mat srcImage = imread("../Resources/me_org.jpg", IMREAD_GRAYSCALE);
-    namedWindow("image[origin]",1);
-    imshow("image[origin]",srcImage);
+	vector<String> fileName;
 
-    //检测surf关键点、提取训练图像描述符
-    vector<KeyPoint> keyPoint;
-    Mat descriptor;
-Ptr<SURF> featureDetector;
-    featureDetector = SURF::create(80);
-featureDetector->detectAndCompute(srcImage, Mat(), keyPoint, descriptor);
-//    featureDetector->detect(grayImage,keyPoint);
-//    SurfDescriptorExtractor featureExtractor;
-//    featureDetector->compute(grayImage, keyPoint, descriptor);
+	fileName.push_back("../Resources/off_cut_scal.jpg");
+	fileName.push_back("../Resources/off_b_s.jpg");
 
-    //创建基于FLANN的描述符匹配对象
-    FlannBasedMatcher matcher;
-    vector<Mat> desc_collection(1,descriptor);
-    matcher.add(desc_collection);
-    matcher.train();
+	Mat img1 = imread(fileName[0], IMREAD_GRAYSCALE);
+	Mat img2 = imread(fileName[1], IMREAD_GRAYSCALE);
 
-    //创建视频对象、定义帧率
-    VideoCapture capture;
-    capture.open(0);
-    unsigned int frameCount=0;//帧数
+	Ptr<SURF> surf;
+	surf = SURF::create(800);
 
-    //不断循环，直到q键被按下
-    while(char(waitKey(1))!=27)
-    {
-        //参数设置
-        int64 time0=getTickCount();
-        Mat testImage,grayImage_test;
-        capture >> testImage;//采集视频到testImage中
-        if(testImage.empty())
-            continue;
-        //转换图像到灰度
-        cvtColor(testImage, grayImage_test, COLOR_BGR2GRAY);
-        //检测S关键点、提取测试图像描述符
-        vector<KeyPoint> keyPoint_test;
-        Mat descriptor_test;
-featureDetector->detectAndCompute(grayImage_test, Mat(), keyPoint_test, descriptor_test);
-//        featureDetector->detect(grayImage_test, keyPoint_test);
-//        featureExtractor->compute(grayImage_test, keyPoint_test, descriptor_test);
+	BFMatcher matcher;
+	// Descriptor for img1 and img2
+	Mat descImg1, descImg2;
+	// keypoint  for img1 and img2
+	vector<KeyPoint> keyImg1, keyImg2;
+	// Match between img1 and img2
+	vector<DMatch> matches;
 
-        //匹配训练和测试描述符
-        vector< vector<DMatch> > matches;
-        matcher.knnMatch(descriptor_test, matches, 2);
-        //根据劳氏算法，得到优秀的匹配点
-        vector<DMatch> goodMatches;
-        for(unsigned int i=0;i<matches.size();++i)
-        {
-            if(matches[i][0].distance<0.6*matches[i][1].distance)
-                goodMatches.push_back(matches[i][0]);
-        }
-        //绘制匹配点并显示窗口
-        Mat dstImage;
-        drawMatches(testImage, keyPoint_test, srcImage, keyPoint, goodMatches, dstImage);
-        namedWindow("image[match]",1);
-        imshow("image[match]",dstImage);
+	surf->detectAndCompute(img1, Mat(), keyImg1, descImg1);
+	surf->detectAndCompute(img2, Mat(), keyImg2, descImg2);
 
-        //输出帧率信息
-        cout << "当前帧率为：" <<getTickFrequency()/(getTickCount()-time0) << endl;
+	matcher.match(descImg1, descImg2, matches);
+
+	sort(matches.begin(), matches.end());
+
+	vector<DMatch> bestMatches;
+	int ptsPairs = std::min(50, (int)(matches.size() * 0.15));
+	cout << ptsPairs << endl;
+	for (int i = 0; i < ptsPairs; i++) {
+		bestMatches.push_back(matches[i]);
+	}
+
+Mat result;  
+    drawMatches(img1, keyImg1, img2, keyImg2, bestMatches, result);//, Scalar::all(-1), Scalar::all(-1),vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS
+
+std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+  
+    for (size_t i = 0; i < bestMatches.size(); i++)   {
+        obj.push_back(keyImg1[bestMatches[i].queryIdx].pt);
+        scene.push_back(keyImg2[bestMatches[i].trainIdx].pt);
     }
-    return 0;
+
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = Point(0, 0);
+    obj_corners[1] = Point(img1.cols, 0);
+    obj_corners[2] = Point(img1.cols, img1.rows);
+    obj_corners[3] = Point(0, img1.rows);
+    std::vector<Point2f> scene_corners(4);
+
+    Mat H = findHomography(obj, scene, RANSAC);
+    perspectiveTransform(obj_corners, scene_corners, H);  
+
+    line(result,scene_corners[0] + Point2f((float)img1.cols, 0), scene_corners[1] + Point2f((float)img1.cols, 0),Scalar(0, 255, 0), 2, LINE_AA);
+    line(result,scene_corners[1] + Point2f((float)img1.cols, 0), scene_corners[2] + Point2f((float)img1.cols, 0),Scalar(0, 255, 0), 2, LINE_AA);
+    line(result,scene_corners[2] + Point2f((float)img1.cols, 0), scene_corners[3] + Point2f((float)img1.cols, 0),Scalar(0, 255, 0), 2, LINE_AA);
+    line(result,scene_corners[3] + Point2f((float)img1.cols, 0), scene_corners[0] + Point2f((float)img1.cols, 0),Scalar(0, 255, 0), 2, LINE_AA);
+    imshow("surf-compare", result);
+    cvWaitKey(0);
 }
 
